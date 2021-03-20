b0ebfed5246f3a2ca84b81c2eaf2ee962bea064e
DhruvSingh861 <79684954+DhruvSingh861@users.noreply.github.com>
2021-03-20 10:36:47 +0530

    Add files via upload
    Program for creation of Applet using swing.

---
+++ JAppletDemo.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+/*
+<object code="JAppletDemo" width=220 height=90>
+</object>
+*/
+public class JAppletDemo extends JApplet{
+public void init(){
+setLayout(new FlowLayout());
+JLabel l=new JLabel("JAPPLEYT");
+add(l);
+//jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+}
